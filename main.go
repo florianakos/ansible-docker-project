1ae70c357f1ea65a9c97f8f8362aa7f69996bbb7
Flo Rian <florian.akos.szabo@gmail.com>
2019-11-23 19:40:57 +0100

    commit and test on lenovo

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ import (
 const openFileOptions int = os.O_CREATE | os.O_RDWR
 const openFilePermissions os.FileMode = 0660
 
-func openFile(f string) (*os.File, error) {
-	newName := "archive/" + strings.ReplaceAll(f, "/", "_") + ".gz"
-	return os.OpenFile(newName, openFileOptions, openFilePermissions)
-}
-
 func closeFile(handle *os.File) {
 	if handle == nil {
 		return
@@ -38,43 +33,81 @@ func readFile(s string) []byte {
 	return bts
 }
 
-func zipFile(filePath string) {
-	handle, err := openFile(filePath[10:])
+func logItAll(msg string) {
+	log.Print(msg)
+	f, err := os.OpenFile("service_history.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Println("[ERROR] Opening file:", err)
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(msg); err != nil {
+		log.Println(err)
+	}
+}
+
+func percentage(old, new float64) (delta float64) {
+	diff := float64(old - new)
+	delta = (diff / float64(old)) * 100
+	return
+}
+
+func compressionRateCalc(old string, new string) {
+	f1, err := os.Open(old)
+	if err != nil {
+		return
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(new)
+	if err != nil {
+		return
+	}
+	defer f2.Close()
+
+	s1, err := f1.Stat()
+	if err != nil {
+		return
+	}
+
+	s2, err := f2.Stat()
+	if err != nil {
+		return
+	}
+	oldSize := float64(s1.Size() + 0.0)
+	newSize := float64(s2.Size() + 0.0)
+
+	logItAll(fmt.Sprintf("File: '%s' (size: %.2f KB), archived as 'archive/%s' (size: %.2f KB) compression rate: %.4f%%\n", old, oldSize/1024.0, strings.ReplaceAll(new[8:], "/", "_"), newSize/1024.0, percentage(oldSize, newSize)))
+}
+
+func zipAndArchive(filePath string) {
+	oldName := filePath
+	newName := "archive/" + strings.ReplaceAll(filePath[10:], "/", "_") + ".gz"
+
+	handle, err := os.OpenFile(newName, openFileOptions, openFilePermissions)
+	if err != nil {
+		log.Fatalln("[ERROR] Opening file:", err)
+		return
 	}
 	zipWriter, err := gzip.NewWriterLevel(handle, 9)
 	if err != nil {
 		log.Println("[ERROR] New gzip writer:", err)
+		return
 	}
-	numberOfBytesWritten, err := zipWriter.Write(readFile(filePath))
+	_, err = zipWriter.Write(readFile(filePath))
 	if err != nil {
 		log.Println("[ERROR] Writing:", err)
+		return
 	}
 	err = zipWriter.Close()
 	if err != nil {
 		log.Println("[ERROR] Closing zip writer:", err)
+		return
 	}
-	fmt.Println("[INFO] Number of bytes written:", numberOfBytesWritten)
-
+	// fmt.Println("[INFO] Number of bytes written:", numberOfBytesWritten)
 	closeFile(handle)
-
-	///////////////////////////////////////////////////////////////////////////
-	// f, err := os.Create()
-	// if err != nil {
-	// 	return err
-	// }
-	// defer f.Close()
-
-	// bfw := bufio.NewWriter(f)
-	// gw := gzip.NewWriter(bfw)
-	// gw.Name = "asd.txt"
-	// gw.Comment = "an epic space opera by George Lucas"
-	// gw.ModTime = time.Date(1977, time.May, 25, 0, 0, 0, 0, time.UTC)
-	// gw.Write([]byte("LOLLOLOLOLOLO"))
-	// bfw.Flush()
-	// gw.Close()
-	// return nil
+	// calc and log compression compr rate
+	compressionRateCalc(oldName, newName)
 }
 
 func main() {
@@ -93,22 +126,19 @@ func main() {
 				if !ok {
 					return
 				}
-				log.Println("event:", event)
+				// if new file was written into the watched folder
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					// log.Println("sending to compress and archive", event.Name)
-					zipFile(event.Name)
+					zipAndArchive(event.Name)
+				// if new folder was created in watched folder add it as watched too
 				} else if event.Op&fsnotify.Create == fsnotify.Create {
-					fi, err := os.Stat(event.Name)
-					if err != nil {
-						return
-					}
+					fi, _ := os.Stat(event.Name)
 					if mode := fi.Mode(); mode.IsDir() {
 						err = watcher.Add(event.Name)
 						if err != nil {
-							log.Fatal(err)
+							log.Println(err)
+							return
 						}
-					} else {
-						log.Println("No new dir...")
 					}
 				}
 			case err, ok := <-watcher.Errors:
